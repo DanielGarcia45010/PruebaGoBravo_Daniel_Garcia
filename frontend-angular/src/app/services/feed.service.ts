ddcc8322d92dbbae8e2480965157cfef2ba6d1a9
Daniel Alfonso Garcia Pilimur <69726976+DanielGarcia45010@users.noreply.github.com>
2025-08-05 21:33:41 +0000

    feed service

---
+++ frontend-angular/src/app/services/feed.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FeedEntry } from '../models/feed-entry.model';
+
+@Injectable({ providedIn: 'root' })
+export class FeedService {
+  private apiUrl = '/entries.json';
+  constructor(private http: HttpClient) {}
+  getFeedEntries(): Observable<FeedEntry[]> {
+    return this.http.get<FeedEntry[]>(this.apiUrl);
+  }
+}

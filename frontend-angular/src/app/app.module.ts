bb3e652c59db962b4c74b1f3ead8f2f37275f119
Daniel Alfonso Garcia Pilimur <69726976+DanielGarcia45010@users.noreply.github.com>
2025-08-05 21:31:22 +0000

    app module

---
+++ frontend-angular/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { FeedComponent } from './components/feed/feed.component';
+
+@NgModule({
+  declarations: [AppComponent, FeedComponent],
+  imports: [BrowserModule, HttpClientModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}

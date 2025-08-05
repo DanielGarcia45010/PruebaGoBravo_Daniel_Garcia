ae17fc3b21576779994fb12e729dbab761112484
Daniel Alfonso Garcia Pilimur <69726976+DanielGarcia45010@users.noreply.github.com>
2025-08-05 22:23:38 +0000

    componente

--- frontend-angular/src/app/components/feed/feed.component.ts
+++ frontend-angular/src/app/components/feed/feed.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { FeedService } from '../../services/feed.service';
+import { FeedService, FeedEntry } from '../../services/feed.service';
 
 @Component({
   selector: 'app-feed',
-  templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
+  templateUrl: './feed.component.html'
 })
 export class FeedComponent implements OnInit {
-  entries: any[] = [];
+  entries: FeedEntry[] = [];
 
   constructor(private feedService: FeedService) {}
 
   ngOnInit(): void {
-    this.feedService.getEntries().subscribe((data: any[]) => {
-      this.entries = data;
+    this.feedService.getFeedEntries().subscribe({
+      next: data => this.entries = data,
+      error: err => console.error('Error loading feed:', err)
     });
   }
 }

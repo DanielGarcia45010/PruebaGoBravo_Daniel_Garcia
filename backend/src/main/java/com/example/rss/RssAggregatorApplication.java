937a0aaf63b2e663482dff079bd02375ebe24045
Daniel Alfonso Garcia Pilimur <69726976+DanielGarcia45010@users.noreply.github.com>
2025-08-05 21:51:42 +0000

    prueba3

--- backend/src/main/java/com/example/rss/RssAggregatorApplication.java
+++ backend/src/main/java/com/example/rss/RssAggregatorApplication.java
@@ -2,9 +2,12 @@ package com.example.rss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // 
 public class RssAggregatorApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(RssAggregatorApplication.class, args);
     }

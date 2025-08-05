d1086d6c9c077dba345b42266083ee2012525c48
Daniel Garcia <da.garciap1@uniandes.edu.co>
2025-08-05 15:59:06 -0500

    correccion

--- frontend-angular/src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<app-feed></app-feed>',
-})
-export class AppComponent {}
--- frontend-angular/src/app/models/feed-entry.model.ts
+++
@@ -1,9 +0,0 @@
-export interface FeedEntry {
-  title: string;
-  link: string;
-  summary: string;
-  content: string;
-  published: string;
-  imageUrl?: string;
-  visited?: boolean;
-}
